25e27a8ea83340b0787a9d1aef57963cee0f5340
maxcore25 <mkorobov2002@mail.ru>
2022-01-31 12:44:44 +0300

    useLocalStorage Custom Hook created

--- src/contexts/BudgetsContext.js
+++ src/contexts/BudgetsContext.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { v4 as uuidV4 } from 'uuid';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 const BudgetsContext = React.createContext();
 
@@ -7,8 +8,8 @@ export function useBudgets() {
   return useContext(BudgetsContext);
 }
 export const BudgetsProvider = ({ children }) => {
-  const [budgets, setBudgets] = useState([]);
-  const [expenses, setExpenses] = useState([]);
+  const [budgets, setBudgets] = useLocalStorage('budgets', []);
+  const [expenses, setExpenses] = useLocalStorage('expenses', []);
 
   function getBudgetExpenses(budgetId) {
     return expenses.filter(expense => expense.budgetId === budgetId);
