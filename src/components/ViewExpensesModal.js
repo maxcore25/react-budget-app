2ccd2a5ebc0ec950f7dc9650c4103eed31bcddc9
maxcore25 <mkorobov2002@mail.ru>
2022-01-31 17:36:15 +0300

    View Expenses Modal Description and  Small Delete Button added

--- src/components/ViewExpensesModal.js
+++ src/components/ViewExpensesModal.js
@@ -4,11 +4,13 @@ import {
   UNCATEGORIZED_BUDGET_ID,
   useBudgets,
 } from '../contexts/BudgetsContext';
+import { currencyFormatter } from '../utils';
 
 export default function ViewExpensesModal({ budgetId, handleClose }) {
   const { getBudgetExpenses, budgets, deleteBudget, deleteExpense } =
     useBudgets();
 
+  const expenses = getBudgetExpenses(budgetId);
   const budget =
     UNCATEGORIZED_BUDGET_ID === budgetId
       ? { name: 'Uncategorized', id: UNCATEGORIZED_BUDGET_ID }
@@ -33,7 +35,21 @@ export default function ViewExpensesModal({ budgetId, handleClose }) {
           </Stack>
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body></Modal.Body>
+      <Modal.Body>
+        <Stack direction='vertical' gap='3'>
+          {expenses.map(expense => (
+            <Stack direction='horizontal' gap='2' key={expense.id}>
+              <div className='me-auto fs-4'>{expense.description}</div>
+              <div className='fs-5'>
+                {currencyFormatter.format(expense.amount)}
+              </div>
+              <Button size='sm' variant='outline-danger'>
+                &times;
+              </Button>
+            </Stack>
+          ))}
+        </Stack>
+      </Modal.Body>
     </Modal>
   );
 }
