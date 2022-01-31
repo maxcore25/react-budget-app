440940661e241979c8e75bcbde3110452a26b506
maxcore25 <mkorobov2002@mail.ru>
2022-01-31 13:09:59 +0300

    Add Budget Modal created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import { Button, Container, Stack } from 'react-bootstrap';
+import AddBudgetModal from './components/AddBudgetModal';
 import BudgetCard from './components/BudgetCard';
 
 function App() {
   return (
-    <Container className='my-4'>
-      <Stack direction='horizontal' gap='2' className='mb-4'>
-        <h1 className='me-auto'>Budgets</h1>
-        <Button variant='primary'>Add Budget</Button>
-        <Button variant='outline-primary'>Add Expense</Button>
-      </Stack>
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
-          gap: '1rem',
-          alignItems: 'flex-start',
-        }}>
-        <BudgetCard name='Entertainment' amount={200} max={1000}></BudgetCard>
-      </div>
-    </Container>
+    <>
+      <Container className='my-4'>
+        <Stack direction='horizontal' gap='2' className='mb-4'>
+          <h1 className='me-auto'>Budgets</h1>
+          <Button variant='primary'>Add Budget</Button>
+          <Button variant='outline-primary'>Add Expense</Button>
+        </Stack>
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+            gap: '1rem',
+            alignItems: 'flex-start',
+          }}>
+          <BudgetCard name='Entertainment' amount={200} max={1000}></BudgetCard>
+        </div>
+      </Container>
+      <AddBudgetModal show />
+    </>
   );
 }
 
